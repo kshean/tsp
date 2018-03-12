bb5f51eb9542dd4fa13dd6d94dbdc2db5510af1f
Shean Krolicki <sheankrolicki@gmail.com>
2018-03-12 17:20:14 -0400

    remove unwanted csv files

--- R/preProcessing.R
+++ R/preProcessing.R
@@ -1,9 +1,9 @@
-test <- read.csv('/csv/test.csv', header = TRUE, col.names = c("name", "address", "lat", "long"))
+input <- read.csv('/csv/input.csv', header = TRUE, col.names = c("name", "address", "lat", "long"))
 
 library(sp)
 library(geosphere)
-x <- test$lat
-y <- test$long
+x <- input$lat
+y <- input$long
 
 xy <- SpatialPointsDataFrame(matrix(c(x,y), ncol=2), data.frame(ID=seq(1:length(x))), proj4string=CRS("+proj=longlat +ellps=WGS84 +datum=WGS84"))
 
@@ -15,6 +15,6 @@ xy$clust <- cutree(hc, k=4)
 
 temp <- cbind(mdist, xy$clust)
 
-data <- data.frame(xy$ID, test$name, test$address);
+data <- data.frame(xy$ID, input$name, input$address);
 write.csv(data, file="/csv/reference.csv", quote = FALSE, row.names = FALSE);
 write.table(temp, file="/csv/master.csv", quote = FALSE, col.names = FALSE)
